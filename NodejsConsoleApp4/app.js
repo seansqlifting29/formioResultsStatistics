83c111c329e4f317d3efa651e38d5655b35e6b65
seansqlifting29 <styndale.gtalum@gmail.com>
2023-03-15 20:01:39 +0800

    Cut code down back to one function. Uses double for loop with if else statement that spits out all attributes and their associated values, and counts the values for percentages. This can be used to find frequency as well.

--- NodejsConsoleApp4/app.js
+++ NodejsConsoleApp4/app.js
@@ -1,16 +1,3 @@
-function dumpProps(obj) {
-    for (const i in obj) {
-        console.log(`${i} = ` + obj[i]);
-    }
-}
-
-let car = {
-        "Make": "Chevy",
-        "Model": "Ford",
-        "Year": "2021",
-        "Color": "Blue"
-    }
-
 var formioResult1 = {
         "data": {
             "checkbox": true,
@@ -21,39 +8,65 @@ var formioResult1 = {
 }
 var formioResult2 = {
     "data": {
-        "checkbox": true,
+        "checkbox": false,
         "checkbox1": false,
         "submit": false
     },
     "metadata": {}
 }
-
-var formioResults = [formioResult1, formioResult2];
-
-function formioStatsT(arr) {
-    var c = 0;
-    for (let i in arr) {
-        for (let i2 in arr[i].data)
-            if (arr[i].data[i2] === true) {
-                c++;
-                console.log(`${i2} = ` + arr[i].data[i2])
-            }
-    }
-    console.log(c);
+var formioResult3 = {
+    "data": {
+        "checkbox": false,
+        "checkbox1": true,
+        "submit": false
+    },
+    "metadata": {}
 }
-function formioStatsF(arr) {
+var formioResult4 = {
+    "data": {
+        "checkbox": true,
+        "checkbox1": true,
+        "submit": false
+    },
+    "metadata": {}
+}
+
+var formioResults = [formioResult1, formioResult2, formioResult3, formioResult4];
+
+function formioStats(arr) {
+    var c = 0;                     // Establish count for true (c) and false (d)
     var d = 0;
-    for (let i in arr) {
-        for (let i2 in arr[i].data)
-            if (arr[i].data[i2] === false) {
+    for (let i in arr) {           // for loop (let i in each of the objects in the array (arr) input into our function; start i = 0, i < length of arr, and i increment by 1 for:
+        for (let i2 in arr[i].data) {     // for loop (let i2 represent the attributes mapping to a boolean value from the data attribute inside each of the objects within each of the objects of arr; input into our function; start i2 = 0, i2 < length of object belonging to data attribute, and i increment by 1)
+            if (arr[i].data[i2] === true) {      //upon completion of one loop, the loop will filter through each attribute and will add to either var c if boolean value is true or to var d if boolean is false. 
+//                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (true)
+                c++;
+            } else {
+//                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (false)
                 d++;
-                console.log(`${i2} = ` + arr[i].data[i2])
             }
+        }
     }
-    console.log(d);
+    let total = c + d;
+    let percentTrue = Math.round((c / total)*100) + '% True';
+    let percentFalse = Math.round((d / total)*100) + '% False';
+    console.log('Number of true = ' + c);
+    console.log('Number of false = ' + d);
+    console.log(percentTrue, percentFalse);
 }
+const b = formioStats(formioResults);
 
+/*let car = {
+        "Make": "Chevy",
+        "Model": "Ford",
+        "Year": "2021",
+        "Color": "Blue"
+    }
+    
+    function dumpProps(obj) {
+    for (const i in obj) {
+        console.log(`${i} = ` + obj[i]);
+    }
+}
 let a = dumpProps(car);
-let b = formioStatsT(formioResults);
-let d = formioStatsF(formioResults);
-//d = console.log(`${i2} = ` + arr[i].data[i2])
+*/
