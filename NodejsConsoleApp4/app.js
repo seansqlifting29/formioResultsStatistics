a05c36a7a3387260a866d4b3921209375611b2c9
seansqlifting29 <styndale.gtalum@gmail.com>
2023-03-17 19:23:43 +0800

    Went and added changes to the file so that the statistics will be based on the attributes instead of the true or false statements. Added .push to create an array of all the attributes associated to the true boolean values. Next, I will need to be able to run basic statistics from this array.

--- NodejsConsoleApp4/app.js
+++ NodejsConsoleApp4/app.js
@@ -1,10 +1,10 @@
 var formioResult1 = {
-        "data": {
-            "checkbox": true,
-            "checkbox1": false,
-            "submit": false
-        },
-        "metadata": {}
+    "data": {
+        "checkbox": true,
+        "checkbox1": false,
+        "submit": false
+    },
+    "metadata": {}
 }
 var formioResult2 = {
     "data": {
@@ -32,17 +32,17 @@ var formioResult4 = {
 }
 
 var formioResults = [formioResult1, formioResult2, formioResult3, formioResult4];
-
-function formioStats(arr) {
+var newObj = [];                    // Creates an empty array. We will use this, and push all of the keys(attributes) that contain true boolean values into it.
+let a = [];
+function formioStatsCheckbox(arr) {
     var c = 0;                     // Establish count for true (c) and false (d)
     var d = 0;
-    for (let i in arr) {           // for loop (let i in each of the objects in the array (arr) input into our function; start i = 0, i < length of arr, and i increment by 1 for:
-        for (let i2 in arr[i].data) {     // for loop (let i2 represent the attributes mapping to a boolean value from the data attribute inside each of the objects within each of the objects of arr; input into our function; start i2 = 0, i2 < length of object belonging to data attribute, and i increment by 1)
+    for (var i in arr) {           // for loop (let i in each of the objects in the array (arr) input into our function; start i = 0, i < length of arr, and i increment by 1 for:
+        for (var i2 in arr[i].data) { // for loop (let i2 represent the attributes mapping to a boolean value from the data attribute inside each of the objects within each of the objects of arr; input into our function; start i2 = 0, i2 < length of object belonging to data attribute, and i increment by 1)
             if (arr[i].data[i2] === true) {      //upon completion of one loop, the loop will filter through each attribute and will add to either var c if boolean value is true or to var d if boolean is false. 
-//                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (true)
-                c++;
+                c++;                              //console.log(Object.keys(object1));
+                newObj.push(i2) 
             } else {
-//                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (false)
                 d++;
             }
         }
@@ -54,8 +54,12 @@ function formioStats(arr) {
     console.log('Number of false = ' + d);
     console.log(percentTrue, percentFalse);
 }
-const b = formioStats(formioResults);
+const b = formioStatsCheckbox(formioResults);
+console.log(newObj);
 
+
+//                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (true)
+//                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (false)
 /*let car = {
         "Make": "Chevy",
         "Model": "Ford",
