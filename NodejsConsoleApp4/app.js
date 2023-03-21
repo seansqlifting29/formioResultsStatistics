2d472bdf94cb8592ee01f1c888922328ee2149b2
seansqlifting29 <styndale.gtalum@gmail.com>
2023-03-21 19:03:47 +0800

    Formatted code to specify statistics to each individual checkbox. checkbox parameter is now passed through in function parameter as checkBoxId.

--- NodejsConsoleApp4/app.js
+++ NodejsConsoleApp4/app.js
@@ -1,62 +1,76 @@
 var formioResult1 = {
     "data": {
-        "checkbox": true,
-        "checkbox1": false,
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
         "submit": false
     },
     "metadata": {}
 }
 var formioResult2 = {
     "data": {
-        "checkbox": false,
-        "checkbox1": false,
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
         "submit": false
     },
     "metadata": {}
 }
 var formioResult3 = {
     "data": {
-        "checkbox": false,
-        "checkbox1": true,
-        "submit": false
-    },
-    "metadata": {}
-}
-var formioResult4 = {
-    "data": {
-        "checkbox": true,
-        "checkbox1": true,
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
         "submit": false
     },
     "metadata": {}
 }
 
-var formioResults = [formioResult1, formioResult2, formioResult3, formioResult4];
-var newObj = [];                    // Creates an empty array. We will use this, and push all of the keys(attributes) that contain true boolean values into it.
-let a = [];
-function formioStatsCheckbox(arr) {
-    var c = 0;                     // Establish count for true (c) and false (d)
-    var d = 0;
+
+
+var formioResults = [formioResult1, formioResult2, formioResult3];
+function formioStatsCheckbox(arr, checkBoxId) {
+    var t = 0;                     // Establish count for true (c) and false (d)
+    var f = 0; 
     for (var i in arr) {           // for loop (let i in each of the objects in the array (arr) input into our function; start i = 0, i < length of arr, and i increment by 1 for:
-        for (var i2 in arr[i].data) { // for loop (let i2 represent the attributes mapping to a boolean value from the data attribute inside each of the objects within each of the objects of arr; input into our function; start i2 = 0, i2 < length of object belonging to data attribute, and i increment by 1)
-            if (arr[i].data[i2] === true) {      //upon completion of one loop, the loop will filter through each attribute and will add to either var c if boolean value is true or to var d if boolean is false. 
-                c++;                              //console.log(Object.keys(object1));
-                newObj.push(i2) 
-            } else {
-                d++;
+        var key = i;
+        var val = arr[i];
+            for (var i2 in arr[i].data) { // for loop (let i2 represent the attributes mapping to a boolean value from the data attribute inside each of the objects within each of the objects of arr; input into our function; start i2 = 0, i2 < length of object belonging to data attribute, and i increment by 1)
+                var sub_key = i2
+                var sub_val = val[i2];
+                if (arr[i].data[i2] === true && sub_key === checkBoxId) {
+                        t++;
+                }
+                if (arr[i].data[i2] === false && sub_key === checkBoxId) {
+                        f++;
+                }
             }
-        }
     }
-    let total = c + d;
-    let percentTrue = Math.round((c / total)*100) + '% True';
-    let percentFalse = Math.round((d / total)*100) + '% False';
-    console.log('Number of true = ' + c);
-    console.log('Number of false = ' + d);
-    console.log(percentTrue, percentFalse);
+    a = ((t * 100) / (t+f)).toFixed(2)
+    console.log(a + `% of population selected ${checkBoxId}`);
 }
-const b = formioStatsCheckbox(formioResults);
-console.log(newObj);
-
+formioStatsCheckbox(formioResults, "over50");
 
 //                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (true)
 //                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (false)
