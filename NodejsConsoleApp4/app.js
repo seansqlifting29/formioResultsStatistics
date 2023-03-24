ad2c3daae35d222609901cdc3f169cb943fed458
seansqlifting29 <styndale.gtalum@gmail.com>
2023-03-24 17:11:41 +0800

    Formatted code's variables to be specific and intuitive for an opensource project.

--- NodejsConsoleApp4/app.js
+++ NodejsConsoleApp4/app.js
@@ -46,45 +46,358 @@ var formioResult3 = {
     },
     "metadata": {}
 }
+var formioResult4 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult5 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult6 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult7 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult8 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult9 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult10 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult11 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult12 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult13 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult14 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult15 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult16 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult17 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult18 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need updates on programs",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult19 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": true,
+        "textArea": "",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult20 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": true,
+        "underlyingHealthConditions": false,
+        "textArea": "Toning arms worked very well",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult21 = {
+    "data": {
+        "under18": false,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": true,
+        "married": false,
+        "single": true,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": true,
+        "textArea": "No changes needed",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult22 = {
+    "data": {
+        "under18": false,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": false,
+        "married": true,
+        "single": false,
+        "under30": true,
+        "over50": false,
+        "underlyingHealthConditions": true,
+        "textArea": "work on squat form",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult23 = {
+    "data": {
+        "under18": true,
+        "male": true,
+        "female": false,
+        "haveEpilepsy": true,
+        "married": true,
+        "single": false,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": false,
+        "textArea": "Need help in surf techniques",
+        "submit": false
+    },
+    "metadata": {}
+}
+var formioResult24 = {
+    "data": {
+        "under18": true,
+        "male": false,
+        "female": true,
+        "haveEpilepsy": false,
+        "married": false,
+        "single": true,
+        "under30": false,
+        "over50": false,
+        "underlyingHealthConditions": true,
+        "textArea": "Need to surf",
+        "submit": false
+    },
+    "metadata": {}
+}
 
-
-
-var formioResults = [formioResult1, formioResult2, formioResult3];
-function formioStatsCheckbox(arr, checkBoxId) {
-    var t = 0;                     // Establish count for true (c) and false (d)
-    var f = 0; 
-    for (var i in arr) {           // for loop (let i in each of the objects in the array (arr) input into our function; start i = 0, i < length of arr, and i increment by 1 for:
-        var key = i;
-        var val = arr[i];
-            for (var i2 in arr[i].data) { // for loop (let i2 represent the attributes mapping to a boolean value from the data attribute inside each of the objects within each of the objects of arr; input into our function; start i2 = 0, i2 < length of object belonging to data attribute, and i increment by 1)
-                var sub_key = i2
-                var sub_val = val[i2];
-                if (arr[i].data[i2] === true && sub_key === checkBoxId) {
-                        t++;
+var formioResults = [formioResult1, formioResult2, formioResult3, formioResult4, formioResult5, formioResult6, formioResult7, formioResult8, formioResult9, formioResult10, formioResult11, formioResult12, formioResult13, formioResult14, formioResult15, formioResult16, formioResult17, formioResult18, formioResult19, formioResult20, formioResult21, formioResult22, formioResult23, formioResult24];
+function formioStatsCheckbox(formioSubmissions, checkBoxId) {
+    var trueCount = 0;                     // Establish count for true (c) and false (d)
+    var falseCount = 0;
+    for (var key in formioSubmissions) {           // for loop (let i in each of the objects in the array (arr) input into our function; start i = 0, i < length of arr, and i increment by 1 for:
+            for (var sub_key in formioSubmissions[key].data) { // for loop (let i2 represent the attributes mapping to a boolean value from the data attribute inside each of the objects within each of the objects of arr; input into our function; start i2 = 0, i2 < length of object belonging to data attribute, and i increment by 1)
+                if (formioSubmissions[key].data[sub_key] === true && sub_key === checkBoxId) {
+                        trueCount++;
                 }
-                if (arr[i].data[i2] === false && sub_key === checkBoxId) {
-                        f++;
+                if (formioSubmissions[key].data[sub_key] === false && sub_key === checkBoxId) {
+                        falseCount++;
                 }
             }
     }
-    a = ((t * 100) / (t+f)).toFixed(2)
+    a = ((trueCount * 100) / (trueCount + falseCount)).toFixed(2)
     console.log(a + `% of population selected ${checkBoxId}`);
 }
-formioStatsCheckbox(formioResults, "over50");
-
-//                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (true)
-//                console.log(`${i2} = ` + arr[i].data[i2])     //This can be used to present all the attributes with their associated boolean values (false)
-/*let car = {
-        "Make": "Chevy",
-        "Model": "Ford",
-        "Year": "2021",
-        "Color": "Blue"
-    }
-    
-    function dumpProps(obj) {
-    for (const i in obj) {
-        console.log(`${i} = ` + obj[i]);
-    }
-}
-let a = dumpProps(car);
-*/
+formioStatsCheckbox(formioResults, "underlyingHealthConditions");
